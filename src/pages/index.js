6fb686b1ffff58f8862c194d3051d6e19005657c
Dan VC <danvc@ellenmacarthurfoundation.org>
2020-03-05 10:30:56 +0000

    Add more test text to header

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,6 +10,7 @@ const IndexPage = () => {
       <h1>CI / CD Development</h1>
       <p>Now being built on the Gatsby Cloud platform!!</p>
       <p>How friggin cool is that!</p>
+      <p>Test: S3 should pick up this new change, from master</p>
       <hr />
       <pre>
         <code>
