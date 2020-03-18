fd6fcfecedf079aac5c3508f92da4b8e6d4a2e9c
Dan VC <danvc@ellenmacarthurfoundation.org>
2020-03-18 12:44:42 +0000

    Test change

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ const IndexPage = () => {
       <p>Now being built on the Gatsby Cloud platform!!</p>
       <p>How friggin cool is that!</p>
       <p>Test: S3 should pick up this new change, from master</p>
+      <p>Making one last change</p>
       <hr />
       <pre>
         <code>
