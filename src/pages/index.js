6d44e65e9678a6afb3684dc27f0cbf3fb019bc30
Dan VC <danvc@ellenmacarthurfoundation.org>
2020-03-19 12:10:13 +0000

    Making a change for test branch 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,7 @@ const IndexPage = () => {
       <p>Test: S3 should pick up this new change, from master</p>
       <p>Test this</p>
       <p>Making one last change</p>
+      <p>Making a change for test branch 1</p>
       <hr />
       <pre>
         <code>
